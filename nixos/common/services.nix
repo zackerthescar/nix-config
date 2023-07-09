b13d2cb4adc4760e736b91fb55fb67a5548d563d
Shaun Loo <shaunloo10@gmail.com>
2023-07-09 18:45:04 -0500

    firewall: fix tailscale exit node not working
    Signed-off-by: Shaun Loo <shaunloo10@gmail.com>

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ...}:
 {
     services.tailscale.enable = true;
+    networking.firewall.enable = true;
+    networking.firewall.checkReversePath = "loose";
     services.openssh.enable = true;
 }
